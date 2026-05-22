40657044664eb54e2bc33e7f516f8ede466210aa
Ngô Quang Phúc <quangphuc29306@gmail.com>
2026-05-22 23:08:13 +0700

    Add: string-encode-and-decode - submission-5

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-5.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string encode(vector<string>& strs) {
+        string encoded_string;
+        // Có dạng ví dụ:  4#Phuc3#Dep3#Trai
+        for(int i = 0; i < strs.size(); i++){
+            encoded_string += to_string(strs[i].size());
+            encoded_string += '#';
+            encoded_string += strs[i];
+        }
+        return encoded_string;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> decoded_string;
+        int i = 0;
+        while(i < s.size()){
+            int j = i;
+            // Tìm vị trí có kí tự #
+            while(s[j] != '#'){
+                j++;
+            }
+            // Chiều dài xâu cần cắt
+            int len = stoi(s.substr(i, j - i));
+            // Xâu cần cắt
+            string cur =  s.substr(j + 1, len);
+            decoded_string.push_back(cur);
+            // Cập nhất i
+            i = j + 1 + len;
+        }
+        return decoded_string;
+    }
+};
