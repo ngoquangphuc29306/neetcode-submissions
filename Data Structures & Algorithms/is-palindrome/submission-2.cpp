b655f3fb2c391579cb6725837e9b842ea8b0dd4a
Ngô Quang Phúc <quangphuc29306@gmail.com>
2026-05-24 20:20:39 +0700

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool alphaNum(char c){
+        return (c >= 'a' && c <= 'z') ||
+               (c >= 'A' && c <= 'Z') ||
+               (c >= '0' && c <= '9');
+    }
+    bool isPalindrome(string s) {
+        int l = 0, r = s.size() - 1;
+        while(l < r){
+            while(l < r && !alphaNum(s[l])){
+                l++;
+            }
+            while(l < r && !alphaNum(s[r])){
+                r--;
+            }
+
+            if(tolower(s[l]) != tolower(s[r])){
+                return false;
+            }
+
+            l++;
+            r--;
+        }
+        return true;
+    }
+};
