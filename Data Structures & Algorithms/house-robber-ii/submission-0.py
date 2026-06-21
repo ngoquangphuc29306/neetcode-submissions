8b1f28bfc2a932232d759fda6123797c770ba75c
Ngô Quang Phúc <quangphuc29306@gmail.com>
2026-06-21 09:21:24 +0700

    Add: house-robber-ii - submission-0

---
+++ Data Structures & Algorithms/house-robber-ii/submission-0.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+
+        if n == 1:
+            return nums[0]
+        
+        if n == 2:
+            return max(nums[0], nums[1])
+
+        dp1 = [0] * n
+        dp2 = [0] * n
+
+        dp1[0] = nums[0]
+        dp1[1] = max(nums[0], nums[1])
+
+        dp2[1] = nums[1]
+        dp2[2] = max(nums[1], nums[2])
+
+        for i in range(2, n - 1):
+            dp1[i] = max(dp1[i - 1], nums[i] + dp1[i - 2])
+
+        for i in range(3, n):
+            dp2[i] = max(dp2[i - 1], nums[i] + dp2[i - 2])
+
+        return max(dp1[n - 2], dp2[n - 1])
+        
+        
